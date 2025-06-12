742a8877ecdf6f30e0a051f249d22bdf78d785d7
ZahaAnass <zahaanass277@gmail.com>
2025-06-12 16:53:58 +0100

    add http and npm and package

---
+++ 13- npm/13- npm command.js
@@ -0,0 +1,14 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac, linux)
+
+// package.json - manifest file (stores important info about project/dependencies)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
